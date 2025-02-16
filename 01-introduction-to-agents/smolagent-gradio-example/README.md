2d197df7a4e901ab4bf125b942e67c089483dde2
miesnerjacob <miesner.jacob@gmail.com>
2025-02-16 11:16:42 -0500

    built gradio demo using smolagents liubrary

---
+++ 01-introduction-to-agents/smolagent-gradio-example/README.md
@@ -0,0 +1,14 @@
+title: Smol Agent Gradio Example
+emoji: ⚡
+colorFrom: blue
+colorTo: yellow
+sdk: gradio
+sdk_version: 5.15.0
+app_file: app.py
+pinned: false
+tags:
+  - smolagents
+  - agent
+  - smolagent
+  - tool
+  - agent-course
---
+++ 01-introduction-to-agents/smolagent-gradio-example/app.py
@@ -0,0 +1,68 @@
+from smolagents import CodeAgent, HfApiModel, load_tool, tool
+import datetime
+import numpy as np
+import pytz
+import yaml
+from tools.final_answer import FinalAnswerTool
+from tools.visit_webpage import VisitWebpageTool
+from tools.web_search import DuckDuckGoSearchTool
+
+from Gradio_UI import GradioUI
+
+
+@tool
+def square_root(num:int)-> str:
+    """Find the square root of a number
+    Args:
+        num: the number to square root
+    """
+    return np.sqrt(num)
+
+@tool
+def get_current_time_in_timezone(timezone: str) -> str:
+    """A tool that fetches the current local time in a specified timezone.
+    Args:
+        timezone: A string representing a valid timezone (e.g., 'America/New_York').
+    """
+    try:
+        tz = pytz.timezone(timezone)
+        local_time = datetime.datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+        return f"The current local time in {timezone} is: {local_time}"
+    except Exception as e:
+        return f"Error fetching time for timezone '{timezone}': {str(e)}"
+
+
+final_answer = FinalAnswerTool()
+
+image_generation_tool = load_tool("agents-course/text-to-image", trust_remote_code=True)
+
+model = HfApiModel(
+max_tokens=2096,
+temperature=0.5,
+model_id='Qwen/Qwen2.5-Coder-32B-Instruct',
+custom_role_conversions=None,
+)
+
+with open("prompts.yaml", 'r') as stream:
+    prompt_templates = yaml.safe_load(stream)
+    
+agent = CodeAgent(
+    model=model,
+    tools=[final_answer,
+           square_root,
+           get_current_time_in_timezone,
+           VisitWebpageTool(),
+           DuckDuckGoSearchTool(),
+           image_generation_tool
+          ],
+    max_steps=6,
+    verbosity_level=1,
+    grammar=None,
+    planning_interval=None,
+    name=None,
+    description=None,
+    prompt_templates=prompt_templates
+)
+
+
+GradioUI(agent).launch()
